517f7b1283e3b9b6eb82173dacb78823847aad47
Mr.Bi <minh.mrbi@gmail.com>
2021-03-25 23:39:45 +0700

    update phone number login with capcha verify

--- src/components/PhonenumberLogin.jsx
+++ src/components/PhonenumberLogin.jsx
@@ -5,38 +5,87 @@ import {
   InputAdornment,
   Paper,
   TextField,
+  IconButton,
 } from "@material-ui/core";
 import React from "react";
 import CenterFluidLayout from "./CenterFluidLayout";
 import { Controller, useForm } from "react-hook-form";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import HomeIcon from "@material-ui/icons/Home";
+import { useNavigate } from "react-router";
+import { NavLink } from "react-router-dom";
+import { firebaseAuth } from "../firebases/FirebaseIndex";
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const VN_PHONE_PREFIX = "+84";
 
 function PhonenumberLogin() {
-  const handlerSubmit = (e) => {
-    e.preventDefault();
-    console.log(e.target.value);
+  const { control, handleSubmit, errors: fieldErrors } = useForm({
+    mode: "onTouched",
+    reValidateMode: "onChange",
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    let phoneStr = VN_PHONE_PREFIX + data.phone;
+    firebaseAuth
+      .signInWithPhoneNumber(phoneStr, window.recaptchaVerifier)
+      .then((confirmResult) => {
+        console.log(confirmResult);
+      })
+      .catch((err) => console.log(err));
   };
+  const navigate = useNavigate();
+
+  const goBack = () => navigate(-1);
+  const goHome = () => navigate("/");
+
+  const validatePhone = (phoneInput) => {
+    if (phoneInput === undefined || phoneInput.length <= 0) {
+      return "Phone number is required.";
+    }
+    // if (phoneInput.length === 10 && phoneInput.startsWith("0")) {
+    //   return undefined;
+    // }
+    if (phoneInput.length !== 9) {
+      return "Invalid phone number.";
+    }
+    return undefined;
+  };
+
+  React.useEffect(() => {
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+      "verifyCapchaContainer"
+    );
+  });
+
   return (
     <CenterFluidLayout>
-      <form noValidate onSubmit={handlerSubmit}>
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
         <Container maxWidth="xs">
           <Paper>
+            <Box display="flex" justifyContent="space-between">
+              <IconButton aria-label="go back" onClick={goBack} edge="end">
+                <ArrowBackIcon />
+              </IconButton>
+              <IconButton
+                aria-label="go home"
+                onClick={goHome}
+                style={{ alignSelf: "center" }}
+                color="primary"
+                component={NavLink}
+                to="/"
+              >
+                <HomeIcon fontSize="large" />
+              </IconButton>
+              <Box>
+                <IconButton />
+              </Box>
+            </Box>
             <Box display="flex" flexDirection="column" padding={4}>
-              <TextField
-                id="phonenumber"
-                label="Phonenumber"
-                name="phonenumber"
-                // value={}
-                // onChange={}
-                variant="outlined"
-                margin="normal"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">+84</InputAdornment>
-                  ),
-                }}
-              />
-              {/* <Controller
-                name="phonenumber"
+              <Controller
+                name="phone"
                 control={control}
                 defaultValue=""
                 rules={{
@@ -63,14 +112,18 @@ function PhonenumberLogin() {
                     error={!!fieldErrors.phone}
                     InputProps={{
                       startAdornment: (
-                        <InputAdornment position="start">+84</InputAdornment>
+                        <InputAdornment position="start">
+                          {VN_PHONE_PREFIX}
+                        </InputAdornment>
                       ),
                     }}
                   />
                 )}
-              /> */}
+              />
+              <div id="verifyCapchaContainer" />
               <Button
                 type="submit"
+                // id="submitPhonenumberLogin"
                 variant="outlined"
                 size="large"
                 color="primary"
--- src/firebases/FirebaseIndex.js
+++ src/firebases/FirebaseIndex.js
@@ -2,12 +2,30 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/analytics";
 
-const app = firebase.initializeApp({});
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+const app = firebase.initializeApp(firebaseConfig);
 firebase.analytics(app);
 
-const firebaseAuth = firebase.auth();
-firebaseAuth.languageCode("vi");
+const firebaseAuth = app.auth();
+firebaseAuth.languageCode = "vi";
 
-export { firebaseAuth };
+firebaseAuth.onAuthStateChanged((fUser) => {
+  if (fUser) {
+    console.log(fUser);
+  } else {
+    console.log(fUser);
+  }
+});
+
+export { firebaseAuth, app as firebaseApp, firebaseConfig };
 
 export default firebase;
