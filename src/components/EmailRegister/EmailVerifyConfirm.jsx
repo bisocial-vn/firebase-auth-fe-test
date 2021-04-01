eaa318aea78ae2eb57010a63d6be29f3ac946d8a
Mr.Bi <minh.mrbi@gmail.com>
2021-04-01 16:34:36 +0700

    update email verify confirm

---
+++ src/components/EmailRegister/EmailVerifyConfirm.jsx
@@ -0,0 +1,100 @@
+import { Box, InputAdornment, TextField, Typography } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import SendIcon from "@material-ui/icons/Send";
+import { firebaseAuth } from "firebases/FirebaseIndex";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+
+function EmailVerifyConfirm() {
+  const { control, handleSubmit, errors: fieldErrors } = useForm({
+    mode: "onTouched",
+    reValidateMode: "onChange",
+  });
+
+  const [status, setStatus] = React.useState("initial");
+
+  const onEmailVerifyConfirm = (data) => {
+    console.log(data);
+    setStatus("loading");
+    firebaseAuth
+      .signInWithEmailLink(data.email)
+      .then((res) => {
+        localStorage.removeItem("EMAIL_FOR_REGISTER");
+        const firebaseIdToken = res.user.getIdToken(true);
+        console.log(firebaseIdToken);
+        setStatus("complite");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <form noValidate onSubmit={handleSubmit(onEmailVerifyConfirm)}>
+      <Box p={4}>
+        {status === "complite" && (
+          <>
+            <Typography variant="subtitle2" color="primary" align="center">
+              Send email success!
+            </Typography>
+            <Typography variant="subtitle2" color="primary" align="center">
+              Please check your mailbox.
+            </Typography>
+          </>
+        )}
+        <Controller
+          name="email"
+          control={control}
+          defaultValue=""
+          rules={{
+            required: {
+              value: true,
+              message: "Email is required.",
+            },
+            // validate: (value) => validateEmail(value),
+          }}
+          render={({ value, onBlur, onChange }) => (
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              variant="outlined"
+              value={value}
+              onChange={onChange}
+              onBlur={onBlur}
+              fullWidth
+              required
+              autoFocus
+              margin="normal"
+              disabled={status === "loading" || status === "complite"}
+              helperText={
+                fieldErrors.email ? fieldErrors.email.message : undefined
+              }
+              error={!!fieldErrors.email}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <MailOutlineIcon color="primary" />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          )}
+        />
+        <Button
+          variant="outlined"
+          type="submit"
+          color="primary"
+          fullWidth
+          endIcon={status !== "complite" && <SendIcon color="primary" />}
+          size="large"
+          disabled={status === "loading" || status === "complite"}
+        >
+          Verify
+        </Button>
+      </Box>
+    </form>
+  );
+}
+
+export default EmailVerifyConfirm;
--- src/pages/FirebaseActionHandlerPage.jsx
+++ src/pages/FirebaseActionHandlerPage.jsx
@@ -1,36 +1,58 @@
-import CenterFluidLayout from "components/layouts/CenterFluidLayout";
-import React from "react";
 import Container from "@material-ui/core/Container";
+import EmailVerifyConfirm from "components/EmailRegister/EmailVerifyConfirm";
 import BasicPaperLayout from "components/layouts/BasicPaperLayout";
-import { useLocation } from "react-router-dom";
-import queryString from "query-string";
-import Typography from "@material-ui/core/Typography";
+import CenterFluidLayout from "components/layouts/CenterFluidLayout";
 import { firebaseAuth } from "firebases/FirebaseIndex";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function FirebaseActionHandlerPage() {
-  const { search } = useLocation();
-  const parserObj = queryString.parse(search);
-  const { mode, oobCode, apiKey, continueUrl, lang } = parserObj;
-  if (
-    mode === "signIn" &&
-    firebaseAuth.isSignInWithEmailLink(window.location.href)
-  ) {
-    // TODO get email from localStorage or from user if empty
-    // TODO then verify
-    // firebaseAuth.signInWithEmailLink("email");
-  }
+  // const { search } = useLocation();
+  // const parserObj = queryString.parse(search);
+  // const { mode, oobCode, apiKey, continueUrl, lang } = parserObj;
+  const navigate = useNavigate();
+
+  const emailForRegister = localStorage.getItem("EMAIL_FOR_REGISTER");
+
+  const registerWithEmail = async (email) => {
+    const idToken = await firebaseAuth
+      .signInWithEmailLink(email)
+      .then((res) => {
+        const firebaseIdToken = res.user.getIdToken(true);
+        console.log(firebaseIdToken);
+        console.log(res.user);
+        return firebaseIdToken;
+        // TODO send firebaseIdToken to backend
+      })
+      .catch((err) => console.log(err));
+    return idToken;
+  };
+
+  React.useEffect(() => {
+    if (!firebaseAuth.isSignInWithEmailLink(window.location.href)) {
+      navigate("/notfound");
+    }
+    if (emailForRegister) {
+      // const firebaseIdToken = await registerWithEmail(emailForRegister);
+      registerWithEmail(emailForRegister);
+    }
+  }, [emailForRegister, navigate]);
+
+  // if (
+  //   mode === "signIn" &&
+  //   firebaseAuth.isSignInWithEmailLink(window.location.href)
+  // ) {
+  // TODO get email from localStorage or from user if empty
+  // TODO then verify
+  // firebaseAuth.signInWithEmailLink("email");
+  // }
 
   return (
     <>
       <CenterFluidLayout>
         <Container maxWidth="xs">
           <BasicPaperLayout headerText="Firebase action handler">
-            Firebase action handler
-            {mode === "signIn" && (
-              <Typography variant="h1" color="initial">
-                Register finish.
-              </Typography>
-            )}
+            <EmailVerifyConfirm />
           </BasicPaperLayout>
         </Container>
       </CenterFluidLayout>
